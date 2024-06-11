bc60fd3f54a4f97d7f5c25e27308cf297b73cc1a
chaliuz <gonzalo.q.fz@gmail.com>
2024-06-11 16:36:17 -0500

    unix date now is showed as human date

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,13 @@ import React, { useState, useEffect } from 'react'
 import useWebSocket, { ReadyState } from "react-use-websocket"
 import Checkbox from 'react-simple-checkbox';
 
-const initialTodos = [
-  // { id: 1, description: 'Desc' },
-  // { id: 2, description: 'Desc #2' },
-];
-
 const App = () => {
-  const [todos, setTodos] = useState(initialTodos);
+  const [todos, setTodos] = useState([]);
   const [input, setInput] = useState('');
 
   // -----------------------------------------------------------------------------------------------------
   // websocket stuff
   // -----------------------------------------------------------------------------------------------------
-  // const WS_URL = "ws://127.0.0.1:8080"
   const WS_URL = "ws://localhost:8080"
   const { sendJsonMessage, lastJsonMessage, readyState } = useWebSocket(
     WS_URL,
@@ -29,27 +23,20 @@ const App = () => {
     if (readyState === ReadyState.OPEN) {
       sendJsonMessage({
         event: "subscribe",
-        // data: {
-        //   channel: "general-chatroom",
-        // },
-        // event: "message",
-        // data: {
-        //   channel: "general-chatroom",
-        // },
       })
     }
   }, [readyState])
 
   // Run when a new WebSocket message is received (lastJsonMessage)
   useEffect(() => {
+    console.log("there is a new message")
     if (lastJsonMessage !== null) {
-      // console.log(`lastJsonMessage: ${JSON.stringify(lastJsonMessage)}`);
+      console.log(lastJsonMessage)
       setTodos(lastJsonMessage)
       setInput("")
     }
 
   }, [lastJsonMessage])
-
 
   // -----------------------------------------------------------------------------------------------------
 
@@ -59,8 +46,6 @@ const App = () => {
   }
 
   const deleteTodo = (productId) => {
-    // const changedTodos = todos.filter(product => product.id !== productId);
-    // setTodos(changedTodos);
     sendJsonMessage({
       event: "message",
       data: {
@@ -104,13 +89,16 @@ const App = () => {
     })
   }
 
+  const convertDate = (date) => {
+    const converted_date = new Date(parseInt(date)); // convert timestamp to milliseconds and construct Date object
+    return (converted_date.toLocaleString())
+  }
+
+
+
   return (
     <div className="flex flex-col w-screen h-min-[80dvi]">
       <div className="flex flex-col flex-1 justify-center items-center self-center w-[80dvi] m-[3rem]">
-        {/* <span>{lastJsonMessage != null ?? lastJsonMessage}</span> */}
-        {/* <pre> */}
-        {/*   {JSON.stringify(lastJsonMessage, null, 2)} */}
-        {/* </pre> */}
         <input
           type='text'
           placeholder='Escribe una Tarea'
@@ -119,6 +107,7 @@ const App = () => {
           onChange={onChangeInput}
           className="p-2 rounded-md text-xl m-3"
         />
+
         <button
           onClick={() => addNote()}>
           Add
@@ -126,8 +115,9 @@ const App = () => {
 
         {todos?.map(product => (
           <div key={product.id} className="flex flex-row justify-between items-center m-[10px] w-[50%]">
-            <span className="text-xl m-[15px] flex-1">{product.id}</span>
-            <p className="flex-5 ">{product.message}</p>
+            {/* <span className="text-md m-[15px] flex-1">{product.id}</span> */}
+            <span className="text-md m-[15px] flex-1">{(convertDate(product.id))}</span>
+            <p className="flex-5 text-xl">{product.content}</p>
 
             <button className="text-xl mx-[15px] flex-2" onClick={() => deleteTodo(product.id)}>
               Delete
@@ -149,11 +139,11 @@ const App = () => {
         ))
         }
         < br />
-        <br />
+        {/* <br /> */}
 
-        <pre>
-          {JSON.stringify(todos, null, 2)}
-        </pre>
+        {/* <pre> */}
+        {/*   {JSON.stringify(todos, null, 2)} */}
+        {/* </pre> */}
       </div>
     </div>
   )
