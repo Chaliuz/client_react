d34d6c9fd5db7f55e3fd725b0024c13dbfb0cd82
chaliuz <gonzalo.q.fz@gmail.com>
2024-06-11 12:40:31 -0500

    notes CRUD implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,70 +1,130 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+
+import useWebSocket, { ReadyState } from "react-use-websocket"
 
 const initialTodos = [
-  { id: 1, description: 'Desc' },
-  { id: 2, description: 'Desc #2' },
+  // { id: 1, description: 'Desc' },
+  // { id: 2, description: 'Desc #2' },
 ];
 
 const App = () => {
   const [todos, setTodos] = useState(initialTodos);
   const [input, setInput] = useState('');
 
+  // -----------------------------------------------------------------------------------------------------
+  // websocket stuff
+  // -----------------------------------------------------------------------------------------------------
+  // const WS_URL = "ws://127.0.0.1:8080"
+  const WS_URL = "ws://localhost:8080"
+  const { sendJsonMessage, lastJsonMessage, readyState } = useWebSocket(
+    WS_URL,
+    {
+      share: false,
+      shouldReconnect: () => true,
+    },
+  )
+  // Run when the connection state (readyState) changes
+  useEffect(() => {
+    // console.log("Connection state changed")
+    if (readyState === ReadyState.OPEN) {
+      sendJsonMessage({
+        event: "subscribe",
+        // data: {
+        //   channel: "general-chatroom",
+        // },
+        // event: "message",
+        // data: {
+        //   channel: "general-chatroom",
+        // },
+      })
+    }
+  }, [readyState])
+
+  // Run when a new WebSocket message is received (lastJsonMessage)
+  useEffect(() => {
+    if (lastJsonMessage !== null) {
+      // console.log(`lastJsonMessage: ${JSON.stringify(lastJsonMessage)}`);
+      setTodos(lastJsonMessage)
+      setInput("")
+    }
+
+  }, [lastJsonMessage])
+
+
+  // -----------------------------------------------------------------------------------------------------
+
+  // console.log(`Got a new message: ${JSON.stringify(lastJsonMessage, null, 2)}`)
+
   const onChangeInput = e => {
     setInput(e.target.value);
-    console.log(input)
   }
 
-  const deleteProduct = (productId) => {
-    const changedTodos = todos.filter(product => product.id !== productId);
-    setTodos(changedTodos);
+  const deleteTodo = (productId) => {
+    // const changedTodos = todos.filter(product => product.id !== productId);
+    // setTodos(changedTodos);
+    // console.log(productId)
+    sendJsonMessage({
+      event: "message",
+      data: {
+        id: productId,
+        operation: "delete",
+      },
+    })
   }
 
-  const addProduct = (newProduct) => {
-    newProduct.id = Date.now();
-    const changeTodos = [
-      newProduct,
-      ...todos,
-    ];
-    setTodos(changeTodos);
-    setInput("")
+  const addNote = (newProduct) => {
+    sendJsonMessage({
+      event: "message",
+      data: {
+        id: Date.now(),
+        message: input,
+        operation: "create",
+      },
+    })
   }
 
-  const updateProduct = (editProduct) => {
-    const changeTodos = todos.map(product => (
-      product.id === editProduct.id
-        ? editProduct
-        : product
-    ))
-    setTodos(changeTodos);
+  const updateNote = ({ id, message }) => {
+    sendJsonMessage({
+      event: "message",
+      data: {
+        id: id,
+        message: message,
+        operation: "update",
+      },
+    })
   }
 
   return (
-    <div class="flex flex-col w-screen h-min-[80dvi]">
-      <div class="flex flex-col flex-1 justify-center items-center self-center w-[80dvi] m-[3rem]">
+    <div className="flex flex-col w-screen h-min-[80dvi]">
+      <div className="flex flex-col flex-1 justify-center items-center self-center w-[80dvi] m-[3rem]">
+        {/* <span>{lastJsonMessage != null ?? lastJsonMessage}</span> */}
+        {/* <pre> */}
+        {/*   {JSON.stringify(lastJsonMessage, null, 2)} */}
+        {/* </pre> */}
         <input
           type='text'
           placeholder='Escribe una Tarea'
           name='texto'
           value={input}
           onChange={onChangeInput}
-          class="p-2 rounded-md text-xl m-3"
+          className="p-2 rounded-md text-xl m-3"
         />
         <button
-          onClick={() => addProduct({ description: input })}>
+          onClick={() => addNote()}>
           Add
         </button>
 
-        {todos.map(product => (
-          <div key={product.id} class="flex flex-row justify-between items-center m-[10px] w-[50%]">
-            <span class="text-xl m-[15px] flex-1">{product.id}</span>
-            <p class="flex-5 bg-[#acaa03]">{product.description}</p>
+        {todos?.map(product => (
+          <div key={product.id} className="flex flex-row justify-between items-center m-[10px] w-[50%]">
+            <span className="text-xl m-[15px] flex-1">{product.id}</span>
+            <p className="flex-5 ">{product.message}</p>
 
-            <button class="text-xl mx-[15px] flex-2" onClick={() => deleteProduct(product.id)}>
+            <button className="text-xl mx-[15px] flex-2" onClick={() => deleteTodo(product.id)}>
               Delete
             </button>
             <button
-              class="text-xl mx-[5px] flex-2"
-              onClick={() => updateProduct({ id: product.id, description: input })}>
+              className="text-xl mx-[5px] flex-2"
+              onClick={() => updateNote({ id: product.id, message: input })}>
               Update
             </button>
           </div>
